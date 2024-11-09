50fc361800d7da6033bebdee54e38c25e251f63e
Zola <Zelalemyohannes75@gmail.com>
2024-11-09 14:20:24 +0300

    add simple hello

--- app.vue
+++ app.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="flex items-center justify-center min-h-screen bg-gray-100">
+    <h1 class="text-3xl font-bold text-blue-600">Hello</h1>
   </div>
 </template>
+
+<script setup>
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,17 +2,13 @@
 export default {
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  
-  css: ['@/assets/css/tailwind.css'],
-  
-  build: {
-    postcss: {
-      postcssOptions: {
-        plugins: {
-          tailwindcss: {},
-          autoprefixer: {},
-        },
-      },
+
+  css: ['~/assets/css/tailwind.css'],
+
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
     },
-  }
+  },
 }

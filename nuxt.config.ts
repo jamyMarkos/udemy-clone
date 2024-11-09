549cea91c32ac7e7a7c15fca987b2ab3872e0d73
Zola <Zelalemyohannes75@gmail.com>
2024-11-10 02:57:36 +0300

    Build scripts for deployments

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,6 +12,11 @@ export default defineNuxtConfig({
     },
   ],
 
+  app: {
+    baseURL: '/udemy_clone/', // baseURL: '/<repository>/'
+    buildAssetsDir: 'assets', // don't use "_" at the begining of the folder name to avoids nojkill conflict
+  },
+
   postcss: {
     plugins: {
       tailwindcss: {},

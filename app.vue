3c5d8f3db4a56853d3cdec9f4ecb1065e9b67626
Mintesnot M <jamymarcoss47@gmail.com>
2024-11-10 03:54:12 +0300

    fix: deployment error fix

--- app.vue
+++ app.vue
@@ -1,24 +1,8 @@
 <!-- App.vue -->
 <template>
-  <div>
-    <Navbar />
-    <CourseHeader />
-    <CourseFeatures />
-    <Companies />
-    <CourseContent />
-    <Requirments />
-    <FeaturedReview />
-    <Students />
-    <FrequentlyBought />
-    <InstructorReview />
-    <MoreCourses />
-    <Footer />
-
-    <!-- Import Sticky Header Component -->
-    <StickyHeader />
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
 
-<script setup>
-
-</script>
+<script setup></script>
